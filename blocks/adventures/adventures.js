3d838646d3b655383133d99e0bd114f458bda350
Alessandro Giupponi <141017923+alessandrogiupponi@users.noreply.github.com>
2024-08-21 16:49:58 +0200

    Update adventures.js

--- blocks/adventures/adventures.js
+++ blocks/adventures/adventures.js
@@ -1,1 +1,9 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.className = 'adventure-item';
+  });
+}
 
