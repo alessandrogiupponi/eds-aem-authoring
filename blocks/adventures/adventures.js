05051c3e7030ea6a6d4f69d8a91bcdd6182e942d
Alessandro Giupponi <141017923+alessandrogiupponi@users.noreply.github.com>
2024-08-21 18:34:51 +0200

    Update adventures.js

--- blocks/adventures/adventures.js
+++ blocks/adventures/adventures.js
@@ -8,7 +8,9 @@ export default function decorate(block) {
     row.children[2].className = 'adventure-item-price';
     row.firstElementChild.append(row.children[2]);
     row.children[1].className = 'adventure-item-title';
-    row.firstElementChild.querySelectorAll('img').className = 'adventure-item-image';
+    row.firstElementChild.querySelectorAll('img').forEach((img) => {
+      img.className = 'adventure-item-image';
+    });
   });
 }
 
